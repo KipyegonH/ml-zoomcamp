98f934418b2fc157aed05f73cd90bdae9eb646e7
Kipyegon Hillary <kipyegonhillary001@gmail.com>
2024-11-04 23:07:13 +0300

    fifth commit

--- homework/predict_test.py
+++ homework/predict_test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://localhost:9696/score"
+url = "http://44.202.34.244:9696/score"
 
 client = {"job": "student", "duration": 280, "poutcome": "failure"}
 response = requests.post(url,json=client).json()
